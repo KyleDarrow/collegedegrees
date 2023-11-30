0e6471b5f1dd3e9ef6d891ea88c3e4109a4e4d3a
KyleDarrow <149436038+KyleDarrow@users.noreply.github.com>
2023-11-30 16:38:06 -0500

    Update event.js

--- event.js
+++ event.js
@@ -1,4 +1,5 @@
 
+
 function fetchData() {
     // Reference: https://stackoverflow.com/questions/61456020/best-way-to-check-status-code-of-http-response
     //create a fetch request to return promise
@@ -6,7 +7,7 @@ function fetchData() {
         //resolve promise using the response class
         .then((response) => {
             //check status code of response
-            console.log(response.status, response.statusText);
+            console.log("Status code: " + response.status, response.statusText);
             if (!response.ok){
                 throw new Error(`HTML Error: ${response.status}`);
             }
@@ -15,10 +16,27 @@ function fetchData() {
         //process the returned JSON data
         .then(data => {
             console.log("Data:", data);
+            const degrees = data.collegeDegrees;
+            const degreeList = document.getElementById("degreeList");
+            degreeList.innerHTML = "";
+            const degreeInfo = document.getElementById("degreeList");
+            for (let i = 0; i < degrees.length; i++) {
+                const elements = degrees[i];
+                const degree = elements.degree;
+                const degreeInformation = document.createElement("div");
+                degreeInformation.innerHTML = `
+                <p><strong>School:</strong> ${degree.school}</p>
+                <p><strong>Program:</strong> ${degree["program/major"]}</p>
+                <p><strong>Type:</strong> ${degree.type}</p>
+                <p><strong>Year Conferred:</strong> ${degree["year conferred"]}</p>
+                <br>`;
+                degreeList.appendChild(degreeInformation);
+            }
+            degreeInfo.style.display = "flex";
         })
         .catch(error => {
             console.error("Error:", error.message);
-        })
+        });
 }
 
-document.getElementById("degrees").addEventListener("click", fetchData)
+document.getElementById("degrees").addEventListener("click", fetchData);
