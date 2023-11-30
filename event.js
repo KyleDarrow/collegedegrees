852a87a791d3773bb4dd9da68ae3bfc7ed8ff609
KyleDarrow <149436038+KyleDarrow@users.noreply.github.com>
2023-11-29 19:33:46 -0500

    Update event.js

--- event.js
+++ event.js
@@ -1,5 +1,24 @@
-//create a fetch request to return promise
 
-//resolve promise using the response class
-//check status code of response
-//process the returned JSON data
+function fetchData() {
+    // Reference: https://stackoverflow.com/questions/61456020/best-way-to-check-status-code-of-http-response
+    //create a fetch request to return promise
+    fetch("degree.json")
+        //resolve promise using the response class
+        .then((response) => {
+            //check status code of response
+            console.log(response.status, response.statusText);
+            if (!response.ok){
+                throw new Error(`HTML Error: ${response.status}`);
+            }
+            return response.json();
+        })
+        //process the returned JSON data
+        .then(data => {
+            console.log("Data:", data);
+        })
+        .catch(error => {
+            console.error("Error:", error.message);
+        })
+}
+
+document.getElementById("degrees").addEventListener("click", fetchData)
